cac74a5dc24bd807162428ed551fa4f444929717
Jason Wieringa <jason@codesherpas.com>
2013-06-02 12:13:35 -0400

    Revert "add index"
    This reverts commit ed9d8d8f9008287e2e07cd068fdcf2c0c0fb23d7.

--- civichack-webapp/src/main/webapp/js/markets.js
+++ civichack-webapp/src/main/webapp/js/markets.js
@@ -1,6 +1,6 @@
 function markets(lat, lon) {
   $.ajax("/r/market/geo?lat=" + lat + "&lon=" + lon).done(function(obj) {
-    $(obj.stations).each(function(index, o) {
+    $(obj.stations).each(function(o) {
       $('#map_canvas').gmap('addMarker', {
         'position' : new google.maps.LatLng(o.marketDetails.location.lat, o.marketDetails.location.lon),
         'bounds' : true
