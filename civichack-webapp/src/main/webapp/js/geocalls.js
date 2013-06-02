3a66cb42083fb5c4b9d3e5ec36cf69b9355d68f4
Pierce Hanley <hanley.pierce+github@example.com>
2013-06-02 12:30:54 -0400

    possibly final

--- civichack-webapp/src/main/webapp/js/geocalls.js
+++ civichack-webapp/src/main/webapp/js/geocalls.js
@@ -1,9 +1,13 @@
+
+
+var curstations = []; 
+
 function stations(lat, lon) {
 	$.ajax("/r/bike/geo?lat=" + lat + "&lon=" + lon).done(function(obj) {
 		console.log(obj.stations);
 		$.each(obj.stations, function(index,o) {
 			console.log("object: " ,o);
-			$('#map_canvas').gmap('addMarker', {
+			var curstation = $('#map_canvas').gmap('addMarker', {
 				'position' : new google.maps.LatLng(o.lat, o.lon),
 				'icon': "http://www.google.com/intl/en_us/mapfiles/ms/micons/green-dot.png",
 				'bounds' : true
@@ -11,8 +15,11 @@ function stations(lat, lon) {
 				$('#map_canvas').gmap('openInfoWindow', {
 					'content' : o.name
 				}, this);
-			})
-		})
+			});
+			curstations.push(curstation);
+		});
+
+		
 	});
 
 }
@@ -22,15 +29,20 @@ function markets(lat, lon) {
 		console.log(obj);
 		$.each(obj, function(index,o) {
 			console.log("object: " ,o);
-			$('#map_canvas').gmap('addMarker', {
+			var curmarker = $('#map_canvas').gmap('addMarker', {
 				'position' : new google.maps.LatLng(o.marketDetails.location.lat, o.marketDetails.location.lon),
 				'bounds' : true,
 				'icon': "http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png"
 			}).click(function() {
+				$.each(curstations, function(i, o) {
+					o.setVisible(false);
+				});
+				curstations = [];
 				$('#map_canvas').gmap('openInfoWindow', {
 					'content' : o.marketName
 				}, this);
-			})
+				stations(lat,lon);
+			});
 		})
 	});
 
